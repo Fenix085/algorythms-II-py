b2898a7fc390bf6a359777096d21ad1309086e21
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2026-04-02 15:44:07 +0200

    feat: refactor search to return results
    Code returns matches now
    Instead of printing them out
    Tests verify all

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 
 # brute force approach
 def bruteForce(pattern: str, text: str):
+    results = []
     m = len(pattern)
     n = len(text)
-    for i in range(n-m):
+    for i in range(n-m+1):
         j = 0
         while j < m:
             if text[i+j] != pattern[j]:
                 break
             j+=1
         if j == m:
-            print(f"Pattern found. Index: {i}")
+            results.append(i)
+    return results
+
 
 if __name__ == "__main__":
     f = open("stuff/Lord of The Rings (JRR Tolkien).txt")
     text = f.read()
     f.close()
     pattern = "Prologue"
-    bruteForce(pattern, text)
+    print(bruteForce(pattern, text))
--- main_test.py
+++ main_test.py
@@ -1,1 +1,10 @@
 import pytest
+
+from main import bruteForce
+
+def test_bruteForce():
+    assert bruteForce("b", "aabaa") == [2]
+    assert bruteForce("abc", "abccbaabccba") == [0, 6]
+    assert bruteForce("b", "aaaaa") == []
+    assert bruteForce("aa", "aaaaa") == [0, 1, 2, 3]
+    assert bruteForce("a b", "a aa a b aa b") == [5, 10]
